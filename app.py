c0314e4dd9a1f9d388b030b0585ec6163f9a4f86
Bruno Lopes <brunoleonelopes@gmail.com>
2020-12-14 23:18:23 +0000

    change port to 80

--- app.py
+++ app.py
@@ -10,10 +10,5 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
