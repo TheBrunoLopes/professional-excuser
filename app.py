36e9a6fdb8c3e6b2355253a4cae609bfb165b49d
Bruno Lopes <brunoleonelopes@gmail.com>
2020-12-14 23:34:03 +0000

    add excuses

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    excuse = excuses[randint(0, len(excuses))]
+    excuse = excuses[randint(0, len(excuses)-1)]
     return f"Excuse: {excuse}"
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
